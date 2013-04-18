b2a94dea301c6a789abe2603b500d8154d16d07a
topher eric <575956381@qq.com>
2013-04-18 18:12:24 +0800

    add User

--- doudow/protected/pythonfiles/Config.py
+++ doudow/protected/pythonfiles/Config.py
@@ -1,3 +1,8 @@
 import dbUntils
+from sqlalchemy.orm import mapper
+from datetime import datetime
+from sqlalchemy import *
 
-dbsession=dbUntils.dbConnect.dbConnect('mysql+mysqldb','db4free.net','doudow','doudow','doudow','3306').getDbsession()
+dbconn=dbUntils.dbConnect.dbConnect('mysql+mysqldb','db4free.net','doudow','doudow','doudow','3306')
+dbsession=dbconn.getDbsession()
+dbengine=dbconn.getDbengine()
