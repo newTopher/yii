0577253e45921be3e50dc157dd7e9e8d620fe1cb
topher eric <575956381@qq.com>
2013-04-19 08:46:26 +0800

    some change

--- doudow/protected/pythonfiles/userPackage/User.py
+++ doudow/protected/pythonfiles/userPackage/User.py
@@ -4,14 +4,13 @@ import base
 
 class User(base.base):
 
-    userInstant=None
-
     def __init__(self):
         base.base.__init__(self)
         self.userTable=base.Config.Table(
             'dod_user',self.metaData,
-            base.Config.Column('id',base.Config.Integer,primary_key=True),
+            base.Config.Column('id',base.Config.Integer,primary_key=True,autoincrement=True,default=25),
             base.Config.Column('username',base.Config.Unicode(32)),
+            base.Config.Column('name',base.Config.Unicode(32)),
             base.Config.Column('email',base.Config.Unicode(50),unique=True,nullable=False),
             base.Config.Column('password',base.Config.Unicode(32),nullable=False),
             base.Config.Column('sex',base.Config.Integer,nullable=False),
@@ -25,9 +24,7 @@ class User(base.base):
         )
         self.metaData.create_all(self.engine)
         base.Config.mapper(UserModel,self.userTable)
-        self.session.configure(bind=self.engine)
-        query=self.session.query(UserModel).all()
-        print query
+        
 
 
     def validUserName(self,id):
